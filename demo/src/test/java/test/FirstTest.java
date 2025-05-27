f2a7fea4fab3226b7f39a1391e3da4be1179f87c
NickKaralis <s4094252@student.rmit.edu.au>
2025-05-27 18:41:48 +1000

    test setup done, j unit done

---
+++ demo/src/test/java/test/FirstTest.java
@@ -0,0 +1,11 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FirstTest {
+    @Test
+    void testAddition() {
+        assertEquals(2, 1 + 1);
+    }
+}
