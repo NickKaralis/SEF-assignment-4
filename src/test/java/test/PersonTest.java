054582acc86f4d6100412cdf37ed8b01f371a89d
RowanWildmanRMIT~ <s4089669@student.rmit.edu.au>
2025-05-30 13:05:39 +1000

    commiting with a failed test to see if actions works

--- src/test/java/test/PersonTest.java
+++ src/test/java/test/PersonTest.java
@@ -25,6 +25,7 @@ public class PersonTest {
     void testAddPersonValid() {
         boolean result = person.addPerson("78l_f%&cCB", "Steve", "Smith",
                 "15 | Example Street | Melbourne | Victoria | Australia", "01-01-2025");
+        result = false;
         assertTrue(result);
     }
 
@@ -65,7 +66,7 @@ public class PersonTest {
     // -------------------- UPDATE PERSON TESTS --------------------
     // cbs commenting properly
     @Test
-// Helper to prepare file
+    // Helper to prepare file
     private void writeTestPersonToFile(String line) throws IOException {
         BufferedWriter writer = new BufferedWriter(new FileWriter("persons.txt"));
         writer.write(line);
@@ -76,14 +77,14 @@ public class PersonTest {
     @Test
     void testUpdatePersonNoChanges() throws IOException {
         // Prepare test data in file
-        writeTestPersonToFile("77l_f%&cCB,Anthony,Brown,15 | Example Street | Melbourne | Victoria | Australia,19-12-1999,false");
+        writeTestPersonToFile(
+                "77l_f%&cCB,Anthony,Brown,15 | Example Street | Melbourne | Victoria | Australia,19-12-1999,false");
 
         // Perform update - only birthday changes
         boolean result = person.updatePersonalDetails(
-            "77l_f%&cCB", "77l_f%&cCB", "", "", 
-            "15 | Example Street | Melbourne | Victoria | Australia", 
-            "19-12-1992"
-        );
+                "77l_f%&cCB", "77l_f%&cCB", "", "",
+                "15 | Example Street | Melbourne | Victoria | Australia",
+                "19-12-1992");
 
         assertTrue(result);
     }
@@ -91,59 +92,66 @@ public class PersonTest {
     @Test
     void testUpdatePersonValidChanges() throws IOException {
         // Prepare test data in file
-        writeTestPersonToFile("77l_f%&cCB,Anthony,Brown,15 | Example Street | Melbourne | Victoria | Australia,19-12-1999,false");
+        writeTestPersonToFile(
+                "77l_f%&cCB,Anthony,Brown,15 | Example Street | Melbourne | Victoria | Australia,19-12-1999,false");
 
         // Perform update - address change
         boolean result = person.updatePersonalDetails(
-            "77l_f%&cCB", "77l_f%&cCB", "Anthony", "Brown", 
-            "16 | Example Street | Melbourne | Victoria | Australia", 
-            "19-12-1999"
-        );
+                "77l_f%&cCB", "77l_f%&cCB", "Anthony", "Brown",
+                "16 | Example Street | Melbourne | Victoria | Australia",
+                "19-12-1999");
 
         assertTrue(result);
     }
 
-
     @Test
     void testUpdatePersonInvalidFields() {
-        person.addPerson("77l_f%&cCB", "Anthony", "Brown", "15 | Example Street | Melbourne | Victoria | Australia", "19-12-1999");
-        boolean result = person.updatePersonalDetails("77l_f%&cCB", "77l_f%&cCB", "2453", "345654$@", "knakjd|adn2e@$*&", "12");
+        person.addPerson("77l_f%&cCB", "Anthony", "Brown", "15 | Example Street | Melbourne | Victoria | Australia",
+                "19-12-1999");
+        boolean result = person.updatePersonalDetails("77l_f%&cCB", "77l_f%&cCB", "2453", "345654$@",
+                "knakjd|adn2e@$*&", "12");
         assertFalse(result);
     }
 
     @Test
     void testUpdatePersonUnder18ChangeAddressAndID() {
-        person.addPerson("77l_f%&cCZ", "Child", "Smith", "15 | Example Street | Melbourne | Victoria | Australia", "19-01-2009");
-        boolean result = person.updatePersonalDetails("77l_f%&cCZ", "", "", "", "16 | Example Street | Melbourne | Victoria | Australia", "");
+        person.addPerson("77l_f%&cCZ", "Child", "Smith", "15 | Example Street | Melbourne | Victoria | Australia",
+                "19-01-2009");
+        boolean result = person.updatePersonalDetails("77l_f%&cCZ", "", "", "",
+                "16 | Example Street | Melbourne | Victoria | Australia", "");
         assertFalse(result);
     }
 
     @Test
     void testUpdatePersonIDStartsWithEvenNumber() {
-        person.addPerson("28l_f%&cCB", "Alex", "Smith", "15 | Example Street | Melbourne | Victoria | Australia", "19-01-2000");
+        person.addPerson("28l_f%&cCB", "Alex", "Smith", "15 | Example Street | Melbourne | Victoria | Australia",
+                "19-01-2000");
         boolean result = person.updatePersonalDetails("28l_f%&ccb", "48l_f%&XYB", "", "", "", "");
         assertFalse(result);
     }
 
     // -------------------- ADD DEMERIT POINTS TESTS --------------------
-    
+
     @Test
     void testAddDemeritValid() {
-        person.addPerson("39l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia", "01-01-2000");
+        person.addPerson("39l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia",
+                "01-01-2000");
         String result = person.addDemeritPoints(2, "19-01-2025");
         assertEquals("Success", result);
     }
 
     @Test
     void testAddDemeritInvalidValues() {
-        person.addPerson("28l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia", "01-01-2000");
+        person.addPerson("28l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia",
+                "01-01-2000");
         String result = person.addDemeritPoints(-1, "74#$235");
         assertEquals("Failed", result);
     }
 
     @Test
     void testAddDemeritPointsCauseSuspension() {
-        person.addPerson("28l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia", "01-01-2000");
+        person.addPerson("28l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia",
+                "01-01-2000");
         person.addDemeritPoints(5, "01-01-2025");
         person.addDemeritPoints(5, "02-01-2025");
         person.addDemeritPoints(5, "03-01-2025");
@@ -152,17 +160,18 @@ public class PersonTest {
 
     @Test
     void testAddDemeritPointsToSuspendedUser() {
-        person.addPerson("28l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia", "01-01-2000");
+        person.addPerson("28l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia",
+                "01-01-2000");
         person.addDemeritPoints(7, "01-01-2025");
         String result = person.addDemeritPoints(5, "01-02-2025");
-        assertEquals("Success", result); 
+        assertEquals("Success", result);
     }
 
     @Test
     void testAddDemeritInvalidAmountAndDate() {
-        person.addPerson("28l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia", "01-01-2000");
+        person.addPerson("28l_f%&cCB", "Test", "User", "15 | Example Street | Melbourne | Victoria | Australia",
+                "01-01-2000");
         String result = person.addDemeritPoints(7, "01-01-2025");
         assertEquals("Failed", result);
     }
 }
-
